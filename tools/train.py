37a9fc7487370bda540b75caa5d776b1bf33f834
FocueAI <17051325951@163.com>
2024-09-19 11:28:18 +0800

    优化代码结构

--- tools/train.py
+++ tools/train.py
@@ -24,14 +24,27 @@ from tools import contrastive
 from tools.utils import fit_one_epoch
 from tools.callbacks import LossHistory
 
+################################## ------------------------------------- 总体参数设置区域 begin ----------------------------------------- ########################################
+# 是否使用cuda
+use_cuda = True
+# 模型保存的位置
+save_dir = r'./model_weight_9_18'
+# 图像编码器的输入尺寸
+input_shape = [224, 224]
+# batch大小
+batch_size = 2
+# 优化器的类型 ['adam' , 'sgd']
+optimizer_type = 'adam'
+# 学习率调整策略
+lr_schedular_type = 'ReduceLROnPlateau'
 
+################################## ------------------------------------- 总体参数设置区域 end ----------------------------------------- ########################################
 
 # step1: 数据加载模块
 dataset_path = r'dataset'
 train_img_lines, train_text_lines, train_labels, val_img_lines, val_text_lines, val_labels = load_dataset(dataset_path)
 # print(f"train_img_lines:{train_img_lines}")
-input_shape = [224, 224]
-batch_size = 2
+
 ## 训练数据
 train_dataset  = SiameseDataset(input_shape, train_img_lines, train_text_lines, train_labels, True, autoaugment_flag=False)
 train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=dataset_collate )
@@ -42,7 +55,8 @@ val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, c
 
 # step2：模型加载模块
 cli2p_model = CLI2P({}) # TODO: 可以跑通该代码
-cli2p_model = cli2p_model.cuda()
+if use_cuda:
+    cli2p_model = cli2p_model.cuda()
 
 # step3: 损失函数加载模块
 loss_fn = contrastive.ContrastiveLoss()
@@ -51,7 +65,6 @@ loss_fn = contrastive.ContrastiveLoss()
 Init_lr_fit = 3e-5
 momentum      = 0.9
 weight_decay  = 0
-optimizer_type = 'adam'
 optimizer = {
     'adam'  : optim.Adam(cli2p_model.parameters(), Init_lr_fit, betas = (momentum, 0.999), weight_decay = weight_decay),
     'sgd'   : optim.SGD(cli2p_model.parameters(), Init_lr_fit, momentum=momentum, nesterov=True, weight_decay = weight_decay)
@@ -59,14 +72,21 @@ optimizer = {
 
 
 # step5: 
-save_dir = r'./model_weight_9_18'
 loss_history = LossHistory(save_dir, cli2p_model, input_shape=input_shape)
 
 
 
 # step6：学习率调整单元
-schedular = StepLR(optimizer=optimizer,step_size=6,gamma=0.9) # 每 2 轮 学习率 变成原来的 0.9倍
-schedular = ReduceLROnPlateau(optimizer=optimizer, mode='min',factor=0.9, patience=3, eps=1e-10) # 当学习率小于eps之后，学习率将不在调整!!!
+
+
+lr_schedular = {
+    'StepLR': StepLR(optimizer=optimizer,step_size=6,gamma=0.9), # 每 2 轮 学习率 变成原来的 0.9倍
+    'ReduceLROnPlateau': ReduceLROnPlateau(optimizer=optimizer, mode='min',factor=0.9, patience=3, eps=1e-10) # 当学习率小于eps之后，学习率将不在调整!!!
+    
+}[lr_schedular_type]
+
+
+
 
 # step7: 开始训练
 Epoch = 100
@@ -83,8 +103,8 @@ for epoch in range(Epoch):
         per_epoch_train_steps= len(train_img_lines) // batch_size,
         per_epoch_val_steps = len(train_img_lines) // batch_size,
         save_weight_dir = save_dir,
-        use_cuda = True,
+        use_cuda = use_cuda,
     )
-    schedular.step(val_loss)
+    lr_schedular.step(val_loss)
 
 
