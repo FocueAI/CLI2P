7b1e18a6085420964bf1d6ff8430e51b0177197d
FocueAI <17051325951@163.com>
2024-10-17 17:28:57 +0800

    在 feature_mix.ipynb中建立 MultiModalTransformer类，可以跑通，下一个版本要将其调整为可以接入模型做特征融合的替代模块，也就是说做为方案二使用，其中方案一使用的是自编码器方案!!!!!

---
+++ models/feature_mix.py
@@ -0,0 +1,15 @@
+# 打算仿写 Q-former 实现该逻辑、
+import torch
+import torch.nn as nn
+from transformers import BertModel
+
+
+
+class MultimodalTransformer(nn.Module):
+    
+    def __init__(self,):
+        pass
+    
+    def forward(self, im_feature, text_feature):
+        pass
+    
